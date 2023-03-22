1a7abb44f568313fe0bb4511a7f297ff918f4afd
bilgehanars <67799759+bilgehanars@users.noreply.github.com>
2023-03-22 19:04:11 +0300

    RE

--- js/src/admin/components/PackManPage.js
+++ js/src/admin/components/PackManPage.js
@@ -4,42 +4,46 @@ import Alert from 'flarum/common/components/Alert';
 
 export default class PackManPage extends ExtensionPage {
   oninit(vnode) {
-    super.oninit(vnode); 
-    this.komut = "";
-    this.packadi = "";
-    this.output = "";
+    super.oninit(vnode);
+    this.komut = '';
+    this.packadi = '';
+    this.output = '';
     app.request({
-      method: 'post',
-      url: app.forum.attribute('apiUrl') + '/packman',
-      timeout: 100000,
-      body: {
-        komut: 'prohibits',
-        packadi: ''
-      },
-    })
-    .then(result => { // ok işareti ile tanımlanan fonksiyon
-      console.log(result);
-      this.output = ' REQUIRED PACKAGES\n';
-      this.output += result; 
-      m.redraw(); 
-    })
+        method: 'post',
+        url: app.forum.attribute('apiUrl') + '/packman',
+        timeout: 100000,
+        body: {
+          komut: 'prohibits',
+          packadi: '',
+        },
+      })
+      .then((result) => {
+        // ok işareti ile tanımlanan fonksiyon
+        console.log(result);
+        this.output = ' REQUIRED PACKAGES\n';
+        this.output += result;
+        m.redraw();
+      });
   }
 
   content() {
     return (
-      <div className={"container"}>
-        <Alert> While the page is loading, PackMan will automatically print the applications that are required by composer for flarum. It may take a minute. </Alert>
+      <div className={'container'}>
+        <Alert>
+          {' '}
+          While the page is loading, PackMan will automatically print the applications that are required by composer for flarum. It may take a minute.{' '}
+        </Alert>
         <form onsubmit={this.onsubmit.bind(this)}>
           Composer Command - Require/Update/Remove <br />
-          <input className="FormControl" value={this.komut} oninput={e => this.komut = e.target.value}></input> <br />
-          Package Name (Example: Bilgehanars/PackMan)<br />
-          <input className="FormControl" value={this.packadi} oninput={e => this.packadi = e.target.value}></input> <br />
-          <Button id="execute-button" type="submit">Execute</Button>
+          <input className="FormControl" value={this.komut} oninput={(e) => (this.komut = e.target.value)}></input> <br />
+          Package Name (Example: Bilgehanars/PackMan)
+          <br />
+          <input className="FormControl" value={this.packadi} oninput={(e) => (this.packadi = e.target.value)}></input> <br />
+          <Button id="execute-button" type="submit">
+            Execute
+          </Button>
         </form>
-        {m('div', [
-  this.output !== '' && m('div.PackManPage-output', m('pre', this.output))
-])
-        }
+        {m('div', [this.output !== '' && m('div.PackManPage-output', m('pre', this.output))])}
       </div>
     );
   }
@@ -48,23 +52,25 @@ export default class PackManPage extends ExtensionPage {
     var button = document.getElementById('execute-button');
     button.disabled = true;
     e.preventDefault();
-    app.request({
-      method: 'post',
-      url: app.forum.attribute('apiUrl') + '/packman',
-      timeout: 100000,
-      body: {
-        komut: this.komut,
-        packadi: this.packadi
-      },
-    })
-    .then(result => { // ok işareti ile tanımlanan fonksiyon
-      button.disabled = false;
-      console.log(result);
-      this.output = '------OUTPUT------\n';
-      this.output += result + '\n------OUTPUT END------';
-   
-      m.redraw();
-    })
-  } 
+    app
+      .request({
+        method: 'post',
+        url: app.forum.attribute('apiUrl') + '/packman',
+        timeout: 100000,
+        body: {
+          komut: this.komut,
+          packadi: this.packadi,
+        },
+      })
+      .then((result) => {
+        // ok işareti ile tanımlanan fonksiyon
+        button.disabled = false;
+        console.log(result);
+        this.output = '------OUTPUT------\n';
+        this.output += result + '\n------OUTPUT END------';
+
+        m.redraw();
+      });
+  }
 }
 /// composer prohibits flarum/core *
--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,11 +1,7 @@
-
 import app from 'flarum/app';
 
 import PackManPage from './components/PackManPage';
 
 app.initializers.add('bilgehanars-packman', () => {
-  app.extensionData
-    .for('bilgehanars-packman')
-    .registerPage(PackManPage);
-  });
-
+  app.extensionData.for('bilgehanars-packman').registerPage(PackManPage);
+});
